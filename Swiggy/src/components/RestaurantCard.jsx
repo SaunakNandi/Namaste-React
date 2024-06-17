a6dc55797ea2560adeb5a517a2474877f168f3d6
Saunak Nandi <saunaknandi021000@gmail.com>
2024-06-17 22:41:09 +0530

    Restaurant Cards done

--- Swiggy/src/components/RestaurantCard.jsx
+++ Swiggy/src/components/RestaurantCard.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const RestaurantCard = ({resName,cuisine}) => {
+const RestaurantCard = ({resData}) => 
+{
+  const [displayText,setDisplayText]=useState('')
+  useEffect(()=>{
+    const generateText=()=>{
+      let results=resData.info.cuisines.join(', ')
+      if(results.length>25)
+        results=results.substring(0,25)+'...'
+      return results
+    }
+    setDisplayText(generateText())
+  },[])
   return (
     <div className='res-card' style={{ backgroundColor: '#f0f0f0'}}>
-        <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_366/e0vvulfbahjxjz6k4uwi" alt="res-logo"
+        <img src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${resData.info.cloudinaryImageId}`} alt="res-logo"
         className='res-logo'/>
-        <h3>{resName}</h3>
-        <h4>{cuisine}</h4>
-        <h4>4.4 star</h4>
-        <h4>38 minutes</h4>
+        <h3 className='name'>{resData.info.name}</h3>
+        <div >
+        {displayText}
+        </div>
+        <h4>{resData.info.avgRating}</h4>
+        <h4>{resData.info.costForTwo}</h4>
     </div>
   )
 }
