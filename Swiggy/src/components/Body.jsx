f7e5c0bb9b5bea4508ddaf269e6d385fb3285f36
Saunak Nandi <saunaknandi021000@gmail.com>
2024-06-20 08:23:20 +0530

    trying to implement swiggy's api

--- Swiggy/src/components/Body.jsx
+++ Swiggy/src/components/Body.jsx
@@ -1,8 +1,18 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import RestaurantCard from './RestaurantCard'
 import { resObj } from '../utlis/mock_data'
 const Body = () => {
   const [listOfRestaurants,setListOfRestaurants]=useState(resObj)
+
+  useEffect(()=>{
+    const fetchData=async()=>{
+      const data=await fetch(`https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.4878517&lng=88.2832182&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING`)
+      const json=await data.json()
+      console.log(json)
+    }
+    fetchData()
+  },[])
+
   return (
     <div className='body'>
         <div className="filter">
