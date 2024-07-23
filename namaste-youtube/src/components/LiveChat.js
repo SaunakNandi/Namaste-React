e64dca10462f372faf8a0f4120fb257e32ab9d87
Saunak Nandi <saunaknandi021000@gmail.com>
2024-07-24 00:22:00 +0530

    You tube completed

--- namaste-youtube/src/components/LiveChat.js
+++ namaste-youtube/src/components/LiveChat.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import ChatMessage from './ChatMessage'
 import { useDispatch, useSelector } from 'react-redux'
 import { addMessage } from '../utlils/chatSlice'
@@ -8,6 +8,7 @@ import { faker } from '@faker-js/faker'
 const LiveChat = () => {
   const dispatch=useDispatch()
   const chat=useSelector(store=>store.chat.message)
+  const [liveMessage,setMessage]=useState('')
   useEffect(()=>{
     
     const i=setInterval(()=>{
@@ -21,15 +22,34 @@ const LiveChat = () => {
     return ()=> clearInterval(i)
   })
   return (
-    // flex-col-reverse to make the chat come from bottom
-    <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-200 rounded-md overflow-y-auto
-    flex flex-col-reverse'>
-      {
-        chat.map((item)=> 
-          <ChatMessage key={item.id} name={item.name} message={item.message}/>
-        )
-      }
-    </div>
+      // flex-col-reverse to make the chat come from bottom
+    <>
+      <div className='w-full h-[600px] ml-2 p-2 border border-black bg-slate-200 rounded-md overflow-y-auto
+      flex flex-col-reverse'>
+        <div className="">  
+          {
+            chat.map((item)=> 
+              <ChatMessage key={item.id} name={item.name} message={item.message}/>
+            )
+          }
+        </div>
+      </div>
+      <form onSubmit={(e)=>{
+        e.preventDefault()
+        dispatch(addMessage({
+          name:'Nandu',
+          message:liveMessage
+        }))
+        setMessage('')
+      }} 
+      className="w-full p-2 ml-2 border border-black">
+        <input className="w-3/4 border border-black p-1" type="text"
+        value={liveMessage} 
+        onChange={(e)=>setMessage(e.target.value)}/>
+        <button
+        className='m-1 ml-2 p-1 border border-black bg-green-300 rounded-md'><span>Submit</span></button>
+      </form>
+    </>
   )
 }
 
--- namaste-youtube/src/utlils/chatSlice.js
+++ namaste-youtube/src/utlils/chatSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-
+import {OFFSET_LIVE_CHAT} from './constants'
 const chatSlice=createSlice({
     name:'chat',
     initialState:{
@@ -8,9 +8,10 @@ const chatSlice=createSlice({
     reducers:{
         addMessage:(state,action)=>{
             // we have done unshift so that the chat looks like you tube live chat. That is the new chat should appear at the bottom and old chat at the top
-            state.message.unshift(action.payload)
-            if(state.message.length>100)
-                state.message.pop()
+            state.message.splice(OFFSET_LIVE_CHAT,1)
+            state.message.push(action.payload)
+            // if(state.message.length>100)
+            //     state.message.pop()
         }
     }
 })
